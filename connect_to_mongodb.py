2c187de4af7bf5579081136d32fd1435c7f5e8ba
ash322ash422 <160450942+ash322ash422@users.noreply.github.com>
2024-04-07 13:04:42 +0530

    update

--- connect_to_mongodb.py
+++ connect_to_mongodb.py
@@ -6,14 +6,23 @@ print ("host:", host_info)
 
 #########################################
 # one URI string passed as a parameter:
-mongo_client = MongoClient('mongodb://localhost:27017')
+CONNECTION_STRING = 'mongodb://localhost:27017'
+mongo_client = MongoClient(CONNECTION_STRING)
 
 # the string domain and integer port passed:
 mongo_client = MongoClient('localhost', 27017)
 print("################################### Success here###################")
+
 
 myclient = mongo_client
 print(myclient.list_database_names())
 
 dblist = myclient.list_database_names()
 if "mydatabase" in dblist:  print("The database exists.")
+print("###########################")
+
+CONNECTION_STRING = 'mongodb://localhost:27017'
+mongo_client = MongoClient(CONNECTION_STRING)
+db = mongo_client["schools"]
+#db.create_collection("students")
+print(mongo_client.list_database_names())
